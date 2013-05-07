c761784b14dead580f64e0b2c7453ddd7124321c
myowin76 <myowin76@gmail.com>
2013-05-07 19:58:48 +0100

    barebone rails 3 framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nideo::Application.config.secret_token = '56560ff7d5383995712648f77b6556f1407e8d16725b26a50f1478ed355508d47dfcbe3abbee0d6994b7dbe16a60de011e79d8681474329a8a29938a0af7a343'
